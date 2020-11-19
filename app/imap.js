0ae27b3d975edac7d8142ccb8cc310a089c915db
Alex Hoyau <a.hoyau@silexlabs.org>
2020-11-19 09:04:03 +0100

    multiple folders, not only inbox

--- app/imap.js
+++ app/imap.js
@@ -10,7 +10,35 @@ const imap = new Imap({
   port: process.env.IMAP_PORT || 993,
   tls: true
 });
+
+function flattenMailboxes(mailboxes, parentPath = '') {
+  console.log('xxx', mailboxes, parentPath)
+  const arr = Object.keys(mailboxes)
+    .map(name => ({
+      ...mailboxes[name],
+      path: parentPath + name,
+    }) )
+  Object.values(mailboxes)
+    .filter(box => !!box.children)
+    .forEach(box => arr.push(flattenMailboxes(box.children, parentPath + box.delimiter)))
+  return arr
+}
+function getBoxesOfInterest(mailboxes) {
+  return flattenMailboxes(mailboxes)
+    .filter(box => !['\\Drafts', '\\Sent', '\\Junk', '\\Trash'].includes(box.special_use_attrib))
+    .map(box => box.path)
+}
  
+async function getBoxes() {
+  return new Promise((resolve, reject) => {
+    imap.getBoxes((error, mailboxes) => {
+      const result = getBoxesOfInterest(mailboxes)
+      console.log(error, JSON.stringify(result), mailboxes)
+      resolve(result)
+    })
+  })
+}
+
 function openInbox(cb) {
   imap.openBox('INBOX', true, cb);
 }
@@ -33,55 +61,58 @@ exports.fetchMail = async function(numMessages, onMessage) {
       }
     }
 
-    imap.once('ready', function() {
-      openInbox(function(err, box) {
-        if (err) throw err;
-        console.time(`${numMessages} messages`)
-        var f = imap.seq.fetch('1:' + numMessages, {
-          bodies: 'HEADER.FIELDS (FROM TO SUBJECT DATE)',
-          struct: true
-        });
-        f.on('message', function(msg, seqno) {
-          var prefix = '(#' + seqno + ') ';
-          messageQueue[prefix] = { headerDone: false, attrsDone: false}
-          msg.on('body', function(stream, info) {
-            var buffer = '';
-            const size = info.size
-            stream.on('data', function(chunk) {
-              buffer += chunk.toString('utf8');
+    imap.once('ready', async function() {
+      const boxes = await getBoxes()
+      boxes.forEach(boxName => {
+        openInbox(function(err, box) {
+          if (err) throw err;
+          console.time(`${numMessages} messages`)
+          var f = imap.seq.fetch('1:' + numMessages, {
+            bodies: 'HEADER.FIELDS (FROM TO SUBJECT DATE)',
+            struct: true
+          });
+          f.on('message', function(msg, seqno) {
+            var prefix = '(#' + seqno + ') ';
+            messageQueue[prefix] = { headerDone: false, attrsDone: false}
+            msg.on('body', function(stream, info) {
+              var buffer = '';
+              const size = info.size
+              stream.on('data', function(chunk) {
+                buffer += chunk.toString('utf8');
+              });
+              stream.once('end', function() {
+                const header = Imap.parseHeader(buffer)
+                const subject = header.subject ? header.subject[0] : '' // no subject sometimes
+                const from = header.from ? header.from[0] : ''
+                const { to } = header
+                updateMessages(prefix, {
+                  size,
+                  subject, from, to,
+                  headerDone: true,
+                })
+              });
             });
-            stream.once('end', function() {
-              const header = Imap.parseHeader(buffer)
-              const subject = header.subject ? header.subject[0] : '' // no subject sometimes
-              const from = header.from ? header.from[0] : ''
-              const { to } = header
+            msg.once('attributes', function(attrs) {
+              const { date, flags, uid, modseq } = attrs
               updateMessages(prefix, {
-                size,
-                subject, from, to,
-                headerDone: true,
+                date, flags, uid, modseq,
+                attrsDone: true,
               })
             });
+            // msg.once('end', function() {
+            //   console.log(prefix + 'Finished');
+            // });
           });
-          msg.once('attributes', function(attrs) {
-            const { date, flags, uid, modseq } = attrs
-            updateMessages(prefix, {
-              date, flags, uid, modseq,
-              attrsDone: true,
-            })
+          f.once('error', function(err) {
+            reject('Fetch error: ' + err);
           });
-          // msg.once('end', function() {
-          //   console.log(prefix + 'Finished');
-          // });
+          f.once('end', function() {
+            imap.end();
+            console.timeEnd(`${numMessages} messages`)
+            resolve()
+          });
         });
-        f.once('error', function(err) {
-          reject('Fetch error: ' + err);
-        });
-        f.once('end', function() {
-          imap.end();
-          console.timeEnd(`${numMessages} messages`)
-          resolve()
-        });
-      });
+      })
     });
 
     imap.once('error', function(err) {
